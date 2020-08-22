9178a559276b20b2cbc8347c2f2517e86c19a48b
codetologist <codetologist@gmail.com>
2020-08-22 17:21:02 -0400

    Fixing typo in server js file. Wrote required instead of require

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-const path = required('path');
+const path = require('path');
 
 //Connect Database
 connectDB();
