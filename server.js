4eba0cbb26a58c6f4c6f72701d61fee684540049
codetologist <codetologist@gmail.com>
2020-08-22 17:12:52 -0400

    Preparing to deploy to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = required('path');
 
 //Connect Database
 connectDB();
@@ -9,12 +10,20 @@ connectDB();
 app.use(express.json({extended: false}));
 
 //app.get('/', (req,res) => res.send('Hello World'));
-app.get('/', (req,res) => res.json({msg: 'Welcome to the contact keeper api'}));
+//app.get('/', (req,res) => res.json({msg: 'Welcome to the contact keeper api'}));
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//Serve static assets in production. Serve React in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))); //important to put this after the defined routes
+}
 
 const PORT = process.env.PORT || 5000;
 
